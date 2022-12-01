23a5fd78a58961d858d4ff37fd28877025b95f08
Gabriel-Garrido <gabrielgarrido89@gmail.com>
2022-12-01 15:41:21 +0000

    1er commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numeroPintas = Math.floor(Math.random() * 4);
+
+  let pintas = ["♥", "♠", "♣", "♦"];
+  let pintaEstilos = [
+    "card text-center text-danger",
+    "card text-center",
+    "card text-center",
+    "card text-center text-danger"
+  ];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numeroAleatorio = numeros[Math.floor(Math.random() * 13)];
+  let pintaAleatoria = pintas[numeroPintas];
+  let pintaEstilo = pintaEstilos[numeroPintas];
+
+  document.getElementById("pinta1").innerHTML = pintaAleatoria;
+  document.getElementById("pinta2").innerHTML = pintaAleatoria;
+  document.getElementById("numero").innerHTML = numeroAleatorio;
+
+  document
+    .getElementById("carta")
+    .setAttribute("class", "card text-center " + pintaEstilo);
+  console.log(pintaAleatoria);
 };
